ca5edde47315096f30e80eef160b7a9ecd2bca62
Hugo Akindele-Obe <hugo.a.obe@gmail.com>
2024-05-07 12:24:53 +1000

    chat messaging

--- src/IRemoteWhiteboard.java
+++ src/IRemoteWhiteboard.java
@@ -1,14 +1,20 @@
 import shapes.ICustomShape;
 
-import java.awt.*;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public interface IRemoteWhiteboard extends Remote {
 
-    public ArrayList<ICustomShape> getShapes() throws RemoteException;
-    public void addShape(ICustomShape shape) throws RemoteException;
-    public void clearShapes() throws RemoteException;
+    ArrayList<ICustomShape> getShapes() throws RemoteException;
+    void addShape(ICustomShape shape) throws RemoteException;
+    void clearShapes() throws RemoteException;
+
+    ArrayList<String> getCurrentUsers() throws RemoteException;
+    void addUser(String username) throws RemoteException;
+    void kickUser(String username) throws RemoteException;
+    ArrayList<String> getChatMessages() throws RemoteException;
+    void addChatMessage(String message, String user) throws RemoteException;
+    boolean validateUsername(String username) throws RemoteException;
 
 }
--- src/RemoteWhiteboard.java
+++ src/RemoteWhiteboard.java
@@ -3,11 +3,16 @@ import shapes.ICustomShape;
 import java.awt.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RemoteWhiteboard extends UnicastRemoteObject implements IRemoteWhiteboard {
 
     private ArrayList<ICustomShape> shapes = new ArrayList<>();
+    private ArrayList<String> users = new ArrayList<>();
+    private ArrayList<String> chatMessages = new ArrayList<>();
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     protected RemoteWhiteboard() throws RemoteException {
         super();
@@ -27,5 +32,39 @@ public class RemoteWhiteboard extends UnicastRemoteObject implements IRemoteWhit
     @Override
     public void clearShapes() throws RemoteException {
         shapes.clear();
+    }
+
+    @Override
+    public ArrayList<String> getCurrentUsers() throws RemoteException {
+        return users;
+    }
+
+    @Override
+    public void addUser(String username) throws RemoteException {
+        if (!users.contains(username)) {
+            users.add(username);
+            addChatMessage(username + " has joined the whiteboard.", "Server");
+        }
+    }
+
+    @Override
+    public void kickUser(String username) throws RemoteException {
+        users.remove(username);
+        addChatMessage(username + " has departed.", "Server");
+    }
+
+    @Override
+    public ArrayList<String> getChatMessages() throws RemoteException {
+        return chatMessages;
+    }
+
+    @Override
+    public void addChatMessage(String message, String user) throws RemoteException {
+        chatMessages.add("[" + dateFormat.format(new Date()) + "] " + user + ": " + message);
+    }
+
+    @Override
+    public boolean validateUsername(String username) throws RemoteException {
+        return !users.contains(username);
     }
 }
